958327d7005bd4cf4e9066ae66ca6bc2bdf330c9
Jolly Rabbit <jollyrabbitok@gmail.com>
2025-01-01 19:32:56 -0800

    Add new api endpoint

--- api/server.py
+++ api/server.py
@@ -1,11 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
+import logging
+
+# Configure logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+# Note: If you change attribute name `app` for this flask server,
+# make sure you edit the Dockerfile gunicorn command arg too
 app = Flask(__name__)
+
 
 @app.route('/api/v1/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "healthy"})
 
+
+@app.route('/api/v1/messages/latest', methods=['GET'])
+def get_latest_message():
+    """Get latest message"""
+    try:
+        state = request.args.get('state')
+        if not state:
+            return jsonify({'error': 'Missing required field `state`'})  # return Bad request error 400
+        
+        # message = redis_handler.get_latest_message(state)  # TODO: Enable later
+        message = 'haha'
+        return jsonify({'message': message})
+    
+    except Exception as e:
+        message = f'Error getting latest message - {e.args}'
+        logger.error(message)
+        return jsonify({'error': message})  # return Internal server error 500
+
+
+
 # Only if you want to test running directly with Python
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- docs/how-to-setup.md
+++ docs/how-to-setup.md
@@ -1,4 +1,6 @@
 ## How to run locally
+
+### To build and run
 ```sh
 cd <top-level>  # top-level directory containing Dockerfile
 
@@ -11,7 +13,19 @@ docker run -d \
   --env-file .env \
   --name api-server \
   api-server
+
+# Troubleshooting
+docker ps  # Is your container running?
+docker ps -a  # Is your container stopped?
+docker logs api-server  # Look at logs to see why container stopped
 ```
 
-## Test that it's working correctly
+### Test that it's working correctly
 1. Open Url: http://localhost:5000/api/v1/health
+
+
+### Teardown
+```sh
+docker stop api-server
+docker rm api-server
+```
